7c77931859e9f10791e81caef2d99f929835a59f
Himanshu <himanshusharma052008@gmail.com>
2025-04-05 20:31:26 +0530

    SearchBar

--- Frontend/src/pages/Collection.jsx
+++ Frontend/src/pages/Collection.jsx
@@ -9,7 +9,7 @@ const Collection = () => {
   const [filterProducts,setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
   const [subCategory,setSubCategory] = useState([]);
-
+  const [sortType,setSortType] = useState('relevant')
   const toggleCategory = (e)=>{
         if(category.includes(e.target.value)){
           setCategory(prev => prev.filter(item => item !== e.target.value))
@@ -22,7 +22,8 @@ const Collection = () => {
     if(subCategory.includes(e.target.value)){
       setSubCategory(prev => prev.filter(item => item !== e.target.value))
     }else{
-      setCategory(prev => [...prev,e.target.value])
+      setSubCategory(prev => [...prev,e.target.value])
+
     }
   }
 
@@ -32,7 +33,28 @@ const applyFilter = ()=>{
       productsCopy = productsCopy.filter(item => category.includes(item.category))
     }
 
+    if(subCategory.length > 0){
+      productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory))
+    }
+
     setFilterProducts(productsCopy);
+}
+
+const sortProduct = ()=>{
+  let fpCopy = filterProducts.slice();
+  switch(sortType){
+    case 'low-high':
+      setFilterProducts(fpCopy.sort((a,b)=> (a.price - b.price)));
+      break;
+
+    case 'high-low':
+      setFilterProducts(fpCopy.sort((a,b)=>(b.price - a.price)));
+      break;
+
+    default:
+      applyFilter();
+      break;
+  }
 }
 
   useEffect(()=>{
@@ -42,6 +64,10 @@ const applyFilter = ()=>{
   useEffect(()=>{
       applyFilter();
   },[category,subCategory])
+
+ useEffect(()=>{
+  sortProduct();
+ },[sortType])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -70,13 +96,13 @@ const applyFilter = ()=>{
           <p className='mb-3 text-sm font-medium '>Type</p>
           <div className='flex flex-col gap-2 text-sm text-gray-700'>
             <p className='flex gap-2 '>
-              <input className='w-3' type="checkbox" value={'TopWear'} onChange={toggleSubCategory} />TopWear
+              <input className='w-3' type="checkbox" value={'Topwear'} onChange={toggleSubCategory} />TopWear
             </p>
             <p className='flex gap-2 '>
-              <input className='w-3' type="checkbox" value={'BottomWear'} onChange={toggleSubCategory} />BottomWear
+              <input className='w-3' type="checkbox" value={'Bottomwear'} onChange={toggleSubCategory} />BottomWear
             </p>
             <p className='flex gap-2 '>
-              <input className='w-3' type="checkbox" value={'WinterWear'} onChange={toggleSubCategory} />WinterWear
+              <input className='w-3' type="checkbox" value={'Winterwear'} onChange={toggleSubCategory} />WinterWear
             </p>
           </div>
         </div>
@@ -88,7 +114,7 @@ const applyFilter = ()=>{
         <div className='flex justify-between text-base sm:text-2xl mb-4 '>
           <Title text1={'All'} text2={'Collections'}/>
           {/* Product Sort  */}
-          <select className='border-2 border-gray-300 text-sm px-2'>
+          <select onChange={(e)=> setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
             <option value="relavant">Sort by : Relavant</option>
             <option value="low-high">Sort by : Low to High</option>
             <option value="high-low">Sort by : High to Low</option>
