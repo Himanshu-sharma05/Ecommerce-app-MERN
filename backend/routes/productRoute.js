0bacd0ae987bc0af5a1a77e6ad7e0c2a1d114f9c
Himanshu <himanshusharma052008@gmail.com>
2025-04-13 16:06:38 +0530

    fix

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,3 +1,12 @@
 import express from 'express';
 import {listProducts,addProduct,removeProduct,singleProduct} from '../controllers/productController.js';
+import upload from '../middleware/multer.js';
 
+const productRotuer = express.Router();
+
+productRotuer.post('/add',upload.fields({name:`image1`,maxcount:1},{name:`image2`,maxcount:1},{name:`image3`,maxcount:1},{name:`image4`,maxcount:1}),addProduct);
+productRotuer.post('/remove',removeProduct);
+productRotuer.post('/single',singleProduct);
+productRotuer.post('/list',listProducts);
+
+export default productRotuer;
