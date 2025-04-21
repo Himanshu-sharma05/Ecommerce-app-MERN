d868fa60561a5e5d2c16783e6eda84616615e087
Himanshu <himanshusharma052008@gmail.com>
2025-04-21 23:55:42 +0530

    listitem

--- admin/admin/src/pages/List.jsx
+++ admin/admin/src/pages/List.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { backendUrl } from '../App';
+import axios from 'axios'
 const List = () => {
+
+  const [list,setList] = useState([]);
+  const fetchList = async ()=>{
+  try {
+    const response = await axios.get(backendUrl + '/api/product/list');
+    console.log(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+  }
+
+  useEffect(()=>{
+    fetchList();
+  },[])
+
   return (
-    <div>List</div>
+    <div>
+
+    </div>
   )
 }
 
